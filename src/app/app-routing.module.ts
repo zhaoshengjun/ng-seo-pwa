5263d987f68f9625421ef7e3742f03f416ef3fc8
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-12 23:24:23 +1000

    update routing to auto redirect to products page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', component: LayoutComponent, children:[]}
+  {path:'', component: LayoutComponent, children:[
+    {path:'', pathMatch:'full', redirectTo:'/products'},
+    {path:'products',loadChildren:'./product/product.module#ProductModule'}
+  ]}
 ];
 
 @NgModule({
