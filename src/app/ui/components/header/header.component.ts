96e085e85fbc179891ff73e16e4c4e3ede555431
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-12 23:12:45 +1000

    update header

--- src/app/ui/components/header/header.component.ts
+++ src/app/ui/components/header/header.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public logo = 'assets/logo.svg';
+  public title = "Store"
+  public links = [{
+    label:'Products',url:'/products'
+  }]
 
   constructor() { }
 
