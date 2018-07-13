52e540251ee2c8d3a57681c65092cde892dc1de4
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-13 20:55:55 +1000

    new components for products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { UiModule } from './ui/ui.module';
+import { ProductListComponent } from './products/containers/product-list/product-list.component';
+import { ProductDetailComponent } from './products/containers/product-detail/product-detail.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductListComponent, ProductDetailComponent],
   imports: [BrowserModule, AppRoutingModule, UiModule],
   providers: [],
   bootstrap: [AppComponent]
