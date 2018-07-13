44fad3503a7613c3f52aaf55b5fb534208d64a5d
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-13 21:21:11 +1000

    product service

---
+++ src/app/product/services/product.service.ts
@@ -0,0 +1,23 @@
+import { environment } from './../../../environments/environment.prod';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+
+const baseUrl = `${environment.apiUrl}/products`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts():Observable<Product[]> {
+    return this.http.get<Product[]>(baseUrl);
+  }
+
+  getProduct(id: string):Observable<Product> {
+    return this.http.get<Product>(`${baseUrl}/${id}`)
+  }
+}
