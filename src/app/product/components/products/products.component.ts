10766bee546726e36f1be09516edf374f0657a91
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-13 21:35:22 +1000

    use product component inside product list

--- src/app/product/components/products/products.component.ts
+++ src/app/product/components/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-products',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+  @Input() public products: Product[];
   constructor() { }
 
   ngOnInit() {
