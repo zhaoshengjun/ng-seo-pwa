898eeb12c23caff946087270b7db86fb2adddd52
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-13 21:07:09 +1000

    update product list

--- src/app/products/containers/product-list/product-list.component.ts
+++ src/app/products/containers/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../product/models/product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  public products: Product[] = []
 
   constructor() { }
 
