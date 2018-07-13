de743a212c2834daebe05a9148e9f167ee4be212
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-13 21:22:46 +1000

    use product service

--- src/app/products/containers/product-list/product-list.component.ts
+++ src/app/products/containers/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../product/models/product';
+import { ProductService } from '../../../product/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -9,9 +10,10 @@ import { Product } from '../../../product/models/product';
 export class ProductListComponent implements OnInit {
   public products: Product[] = []
 
-  constructor() { }
+  constructor(private productSrv: ProductService) { }
 
   ngOnInit() {
+    this.productSrv.getProducts().subscribe(products => this.products = products);
   }
 
 }
