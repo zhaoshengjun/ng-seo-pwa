d3bf0a5c09bed08e4f8b2b226d7d291dd61db033
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-13 21:32:57 +1000

    new component

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductRoutingModule
   ],
-  declarations: [ProductListComponent, ProductDetailComponent]
+  declarations: [ProductListComponent, ProductDetailComponent, ProductsComponent]
 })
 export class ProductModule { }
