27e9e00677a5225a63afe48fa36a215f6d312e7a
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-14 16:34:37 +1000

    update meta data for product detail page

--- src/app/products/containers/product-detail/product-detail.component.ts
+++ src/app/products/containers/product-detail/product-detail.component.ts
@@ -1,7 +1,8 @@
 import { Product } from "./../../../product/models/product";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
+import { UiService } from "../../../ui/services/ui.service";
 
 @Component({
   selector: "app-product-detail",
@@ -11,13 +12,24 @@ import { map } from "rxjs/operators";
 export class ProductDetailComponent implements OnInit {
   public product: Product = new Product();
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private ui: UiService) {}
 
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get("id");
 
     this.route.data
-      .pipe(map(data => data["product"]))
+      .pipe(
+        map(data => data["product"]),
+        tap(product => this.metaData(product))
+      )
       .subscribe(product => (this.product = product));
+  }
+
+  metaData(product: Product) {
+    this.ui.setMetaData({
+      title: `${product.name} for only ${product.price}`,
+      description: product.description,
+      image: product.image
+    });
   }
 }
