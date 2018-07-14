80af44e9d53b22ad9f4bb5959da80ef62c20997f
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-14 13:55:47 +1000

    update module for server side

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,10 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode } from "@angular/core";
+import "zone.js/dist/zone-node";
 
-import { environment } from './environments/environment';
+import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
 }
 
-export { AppServerModule } from './app/app.server.module';
+export { AppServerModule } from "./app/app.server.module";
