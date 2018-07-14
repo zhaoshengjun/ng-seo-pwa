059e12f4570eee10b0ef4702335b611de5b16da4
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-14 13:58:44 +1000

    module map loader

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { NgModule } from "@angular/core";
+import { ServerModule } from "@angular/platform-server";
 
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
+import { ModuleMapLoaderModule } from "@nguniversal/module-map-ngfactory-loader";
+import { AppModule } from "./app.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-  ],
-  bootstrap: [AppComponent],
+  imports: [AppModule, ServerModule, ModuleMapLoaderModule],
+  bootstrap: [AppComponent]
 })
 export class AppServerModule {}
