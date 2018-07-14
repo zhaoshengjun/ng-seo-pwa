ccc59584e56d3e0b978d815f55fe34db9a720f34
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-14 14:12:22 +1000

    update start script

--- server.ts
+++ server.ts
@@ -1,1 +1,31 @@
 import * as express from "express";
+import { join } from "path";
+
+import { ngExpressEngine } from "@nguniversal/express-engine";
+import { provideModuleMap } from "@nguniversal/module-map-ngfactory-loader";
+
+const PORT = process.env.PORT || 8080;
+const staticRoot = join(process.cwd(), "dist", "ng-seo-pwa");
+const {
+  AppServerModuleNgFactory,
+  LAZY_MODULE_MAP
+} = require("./dist/ng-seo-pwa/main");
+
+const app = express();
+
+app.engine(
+  "html",
+  ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [provideModuleMap(LAZY_MODULE_MAP)]
+  })
+);
+app.set("view engine", "html");
+app.set("views", staticRoot);
+
+app.set("*.*", express.static(staticRoot));
+app.get("*", (req, res) => {
+  res.render("index", { req });
+});
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
